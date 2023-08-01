c460e7455dd250f11af2de307a3e559edfd2b8a0
gregclacker <89424015+gregclacker@users.noreply.github.com>
2023-08-01 18:41:55 -0500

    read me w/ steps on how to contribute

---
+++ readme.md
@@ -0,0 +1,8 @@
+***Audinc*** is a simple java swing applicaiton that hosts miscellaneous tools - of which anyone is welcome to add too. There is no restriction on the purpose of the tool, just that it is written in java and on this repo.
+- because of how the classes are named the 'tools' will be refereed to as "presents"
+- there is no java doc even though some classes have those properties.
+# How to contribute
+1. make a new class inside the <code>presentables.presents</code> package that extends the <code>presentables.Presentable</code> class. the Presentable class will allow your tool to interface with the gui.
+1. copy and paste everyting from the <code>presentables.presentTemplate</code> class into your newly created .java file & change the class name to the correct one.
+1. add your class path to the <code>Presents</code> variable in the <code>audinc.gui.MainWin</code> class.
+1. start making what ever you want.
--- src/audinc/gui/MainWin.java
+++ src/audinc/gui/MainWin.java
@@ -34,10 +34,10 @@ public class MainWin extends JFrame {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	public static String mainTitle = "Audinc";
+	
 	public static Path settingsDir = Paths.get(".\\audincSettings"); // build with ".\\audincSettings"
 	
+	//accessible presents go here. order does not matter
 	public static Set<Class<? extends Presentable>> Presents = Stream.of(
 			presentables.presents.menu.class,
 			presentables.presents.txtToSpeach.class,
@@ -46,16 +46,23 @@ public class MainWin extends JFrame {
 		).collect(Collectors.collectingAndThen(Collectors.toSet(),Collections::<Class<? extends Presentable>>unmodifiableSet));
 	
 	//standard values
-	public static int stdTextSpace = 40,
+	public static int stdTextSpace = 40,	//standard spacing unit between gui components 
 			stdStructSpace = 15,
 			stdPreferredNumThreads = 3;
 	public static float stdDimensionScale = 1.75f;
 	public static Dimension stdDimension = new Dimension((int)(480*stdDimensionScale),(int)(270*stdDimensionScale)),
 			stdtabIconSize = new Dimension((int)(11*stdDimensionScale),(int)(11*stdDimensionScale));
 	
-	public Presentable currPresent = null;
-	
-//	private File settingsFile = new File("settings.audinc");
+	public void setPresent(Class<? extends Presentable> cp) {
+		if(currPresent != null) currPresent.quit();
+		
+		try {
+			Presentable p = cp.getDeclaredConstructor().newInstance(); //throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, SecurityException
+			p.present(this);		
+			setTitle(mainTitle + " : " + Presentable.tryForStatic(p.getClass(), "getDisplayTitle"));
+			currPresent = p; 
+		} catch(InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | SecurityException e) { e.printStackTrace(); }
+	}
 	
 	public MainWin(String title) {
 		super(title);
@@ -82,6 +89,68 @@ public class MainWin extends JFrame {
 		
 		setVisible(true);
 	}
+	
+///////////////////
+//GUI utils
+///////////////////
+	//make a icon from a file
+	public static ImageIcon getImageIcon(String src) {
+			try {
+			return new ImageIcon( ImageIO.read(new File(src)));
+			} catch (IOException e) {
+			return null;
+			}
+	}
+	
+	//make a icon of specified size from a file
+	public static ImageIcon getImageIcon(String src, Dimension size) { return getImageIcon(src, size, Image.SCALE_FAST); }
+	public static ImageIcon getImageIcon(String src, Dimension size, int scaleMethod) {
+		try {
+			Image img = ImageIO.read(new File(src));
+			Image imgScaled = img.getScaledInstance(size.width, size.height, scaleMethod);
+			return new ImageIcon(imgScaled);
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+			return null;
+		}
+	}
+
+///////////////////
+//button events
+///////////////////
+	protected void onAboutClick() {
+		JLabel bg = new JLabel(MainWin.getImageIcon("res/aboutbg.png"));
+
+        JLabel title = new JLabel("<html><body>"
+        +"<h1 align='center'><font size=+1><u>Audinc</u></font></h1>"
+        +"<p align='center'><br>joe cat</p>"
+        +"</body></html>",
+        SwingConstants.CENTER);
+
+        JLabel artists = new JLabel("<html>"
+        +"<br/><p>GDC 3.0 lisence 2023</p>"
+        +"</html>",
+        SwingConstants.CENTER);
+
+        JOptionPane.showMessageDialog(this,
+            new Object[]{bg, title, artists},
+            "Audinc (audi, not the car)",
+            JOptionPane.PLAIN_MESSAGE);
+	}
+	
+	public void quit() {
+		if(currPresent != null) {
+			currPresent.quit();
+		}
+		dispose();
+	}
+
+//////////////////
+// private
+/////////////////
+	private static final long serialVersionUID = 1L;
+	private static String mainTitle = "Audinc";
+	private Presentable currPresent = null;
 	
 	private void initGUIMenuBar() {
 		JMenuBar menubar = new JMenuBar();
@@ -115,68 +184,4 @@ public class MainWin extends JFrame {
 		
 		setJMenuBar(menubar);
 	}
-
-	public void setPresent(Class<? extends Presentable> cp) {
-		if(currPresent != null) currPresent.quit();
-		
-		try {
-			Presentable p = cp.getDeclaredConstructor().newInstance(); //throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, SecurityException
-			p.present(this);		
-			setTitle(mainTitle + " : " + Presentable.tryForStatic(p.getClass(), "getDisplayTitle"));
-			currPresent = p; 
-		} catch(InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException | SecurityException e) { e.printStackTrace(); }
-	}
-	
-///////////////////
-//button events
-///////////////////
-	protected void onAboutClick() {
-		JLabel bg = new JLabel(MainWin.getImageIcon("res/aboutbg.png"));
-
-        JLabel title = new JLabel("<html><body>"
-        +"<h1 align='center'><font size=+1><u>Audinc</u></font></h1>"
-        +"<p align='center'><br>joe cat</p>"
-        +"</body></html>",
-        SwingConstants.CENTER);
-
-        JLabel artists = new JLabel("<html>"
-        +"<br/><p>GDC 3.0 lisence 2023</p>"
-        +"</html>",
-        SwingConstants.CENTER);
-
-        JOptionPane.showMessageDialog(this,
-            new Object[]{bg, title, artists},
-            "Audinc (audi, not the car)",
-            JOptionPane.PLAIN_MESSAGE);
-	}
-	
-	public void quit() {
-		if(currPresent != null) {
-			currPresent.quit();
-		}
-		dispose();
-	}
-
-///////////////////
-//GUI utils
-///////////////////
-	public static ImageIcon getImageIcon(String src) {
-		try {
-			return new ImageIcon( ImageIO.read(new File(src)));
-		} catch (IOException e) {
-			return null;
-		}
-	}
-	public static ImageIcon getImageIcon(String src, Dimension size) { return getImageIcon(src, size, Image.SCALE_FAST); }
-	public static ImageIcon getImageIcon(String src, Dimension size, int scaleMethod) {
-		try {
-			Image img = ImageIO.read(new File(src));
-			Image imgScaled = img.getScaledInstance(size.width, size.height, scaleMethod);
-			return new ImageIcon(imgScaled);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-	
 }
