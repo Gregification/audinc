72d53606ffde99fbb47eed7cae97ba25fc8f2196
gregclacker <yongboone1@gmail.com>
2023-10-20 10:50:26 -0500

    serial poke : rework connection settings editor

--- src/main/java/presentables/presents/serialPoke/SPCSetting.java
+++ src/main/java/presentables/presents/serialPoke/SPCSetting.java
@@ -1,46 +1,117 @@
 package presentables.presents.serialPoke;
 
+import java.util.EnumSet;
+
 public enum SPCSetting {
+		//(String:title, Class:object type, Boolean:allowCustomValue, Boolean:isHowSwappable, String:description, Object[]:choosableValues)
 		SYSTEM_PORT_NAME			("port name", 
-										String.class, false, 	"port name as defined by the system"),
+									String.class, false, 	"port name as defined by the system"),
 		SYSTEM_PORT_PATH 			("port path",
-										String.class, false, 	"port path as defined by the system"),
+									String.class, false, 	"port path as defined by the system"),
 		SYSTEM_PORT_LOCATION		("port location",
-										String.class, false, 	"port location as defined by the system"),
-		DESCRIPTIVE_PORT_NAME		(String.class, false,		"port name as defined by this program"),
+									String.class, false, 	"port location as defined by the system"),
+		DESCRIPTIVE_PORT_NAME		(String.class ,	false,		"port name as defined by this program"),
 		PORT_DESCRIPTION			(Integer.class, false, 		""),
 		PORT_LOCATION				(Integer.class, false, 		""),
-		BAUD_RATE					(Integer.class, true,		"baud rate for communication of this clients connection"),
+		BAUD_RATE					(Integer.class, true ,		"baud rate for communication of this clients connection"),
 		DEVICE_WRITE_BUFFER_SIZE	(Integer.class, false, 		"write size (bytes) for this device"),
 		DEVICE_READ_BUFFER_SIZE		(Integer.class, false,		"read size (bytes) of the this devices buffer"),
 		VENDOR_ID					(Integer.class, false,		""),
 		DATA_BITS_PER_WORD			(Integer.class, false,		""),
-		NUM_STOP_BITS				(Float.class, false,		"", (Object[])(new Float[] {1.5f,1f,2f})),
-		PARITY						(Integer.class, false,		""),
+		NUM_STOP_BITS				(String.class ,	false,		"", (Object[])(stopbitOptions.values())),
+		PARITY						(Integer.class, false,		"", (Object[])(parityOptions.values())),
+		TIMEOUT_READ				(Integer.class, true ,		"time out in miliseconds before a packet is considered lost"),
+		TIMEOUT_WRITE				(Integer.class, true ,		"time out in miliseconds before a write attempt is givenup"),
+		FLOWCONTROL_DATA_SET_READY_ENABLED		("DSR enabled",
+									Boolean.class, true , false, "Data Set Ready"),
+		FLOWCONTROL_DATA_TERMINAL_READY_ENABLED 	("DTR enabled",
+									Boolean.class, true , false,"Data Terminal/Transmission Ready"),
+		FLOWCONTROL_XIN_ONOFF_ENABLED	("IN XonXoff enabled",
+									Boolean.class, true , false,"Input buffer flag signals"),
+		FLOWCONTROL_XOUT_ONOFF_ENABLED	("OUT XonXoff enabled",
+									Boolean.class, true , false,"Output buffer flag signals"),
+		FLOWCONTROL_REQUEST_TO_SEND_ENABLED		("RTS enabled",
+									Boolean.class, true , false,"Request To Send"),
+		FLOWCONTROL_CLEAR_TO_SEND_ENABLED		("CTS enabled",
+									Boolean.class, true , false,"Clear To Send")	
 	;
 	
+	public final Class<? extends Enum> 
+		StopBitOptions 	= stopbitOptions.class,
+		ParityOptions	= parityOptions.class;
+	
 	public String
-		title,
-		description;
+		title				= "default title",
+		description			= "default description";
 	public Object[] choosableValues;
 	public Boolean 
-		allowCustomValues,
-		hotSwappable;
+		allowCustomValues	= false,
+		isHotSwappable		= false;
+	
 	public Class<? extends Object> clas;
+	
+	
+	enum stopbitOptions{
+		ONE_POINT_FIVE_STOP_BITS	("1.5 stop bits"),
+		ONE_STOP_BIT				("1 stop bit"),
+		TWO_STOP_BITS				("2 stop bits")
+		;
+		
+		public String title;
+		
+		private stopbitOptions(String title) 	{ this.title = title; }		
+		@Override public String toString() 		{ return title; }
+	}
+	
+	enum parityOptions{
+			EVEN_PARITY		("even parity"),
+			MARK_PARITY		("mark parity"),
+			NO_PARITY		("no parity"),
+			ODD_PARITY		("odd parity"),
+			SPACE_PARITY	("space parity"),
+		;
+		
+		public String title;
+		
+		private parityOptions(String title) 	{ this.title = title; }		
+		@Override public String toString() 		{ return title; }
+	}
 	
 	private SPCSetting(
 				String title,
 				Class<? extends Object> clas,
 				Boolean allowCustomValues,
+				Boolean isHotSwappable,
 				String description,
 				Object[] choosableValues
 			) {
-		this.title 				= title;
-		this.clas 				= clas;
-		this.allowCustomValues	= allowCustomValues;
-		this.description 		= description;
-		this.choosableValues 	= choosableValues;
+		this(title, clas, allowCustomValues, description, choosableValues);
+		this.isHotSwappable 	= isHotSwappable;
 	}
+	private SPCSetting(
+			String title,
+			Class<? extends Object> clas,
+			Boolean allowCustomValues,
+			Boolean isHotSwappable,
+			String description
+		) {
+	this(title, clas, allowCustomValues, isHotSwappable, description, (Object[])(null));
+}
+	
+	
+	private SPCSetting(
+			String title,
+			Class<? extends Object> clas,
+			Boolean allowCustomValues,
+			String description,
+			Object[] choosableValues
+		) {
+	this.title 				= title;
+	this.clas 				= clas;
+	this.allowCustomValues	= allowCustomValues;
+	this.description 		= description;
+	this.choosableValues 	= choosableValues;
+}
 	
 	private SPCSetting(Class<? extends Object> clas, Boolean chooseableValues, String description, Object[] choosableValues) {
 		this("", clas, chooseableValues, description, choosableValues);
@@ -53,4 +124,39 @@ public enum SPCSetting {
 	private SPCSetting(String title, Class<? extends Object> clas, Boolean chooseableValues, String description) {
 		this(title, clas, chooseableValues, description, (Object[])null);
 	}
+	
+	public static EnumSet<SPCSetting> keepHotSwappables(EnumSet<SPCSetting> set){
+		for(var v : values())
+			if(!v.isHotSwappable) 
+				set.remove(v);		
+		return set;
+	}
 }
+
+/*
+ * c&p
+ * 
+ * SPCSettings
+SYSTEM_PORT_NAME
+SYSTEM_PORT_PATH
+SYSTEM_PORT_LOCATION
+DESCRIPTIVE_PORT_NAME
+PORT_DESCRIPTION
+PORT_LOCATION
+BAUD_RATE
+DEVICE_WRITE_BUFFER_SIZE
+DEVICE_READ_BUFFER_SIZE
+VENDOR_ID
+DATA_BITS_PER_WORD
+NUM_STOP_BITS
+PARITY
+TIMEOUT_READ
+TIMEOUT_WRITE
+FLOWCONTROL_DATA_SET_READY_ENABLED
+FLOWCONTROL_DATA_TERMINAL_READY_ENABLED
+FLOWCONTROL_XIN_ONOFF_ENABLED
+FLOWCONTROL_XOUT_ONOFF_ENABLED
+FLOWCONTROL_REQUEST_TO_SEND_ENABLED
+FLOWCONTROL_CLEAR_TO_SEND_ENABLED
+
+ */
--- src/main/java/presentables/presents/serialPoke/SPCSettings.java
+++ src/main/java/presentables/presents/serialPoke/SPCSettings.java
@@ -1,11 +1,111 @@
 package presentables.presents.serialPoke;
 
+import java.util.EnumSet;
+import java.util.Map;
+
 import com.fazecast.jSerialComm.SerialPort;
 
+//var set = Set.of(setting.StopBitOptions.getEnumConstants());
 public class SPCSettings {
+	public final static EnumSet<SPCSetting> howSwappableSettings = SPCSetting.keepHotSwappables(EnumSet.allOf(SPCSetting.class)); 
+	public final SerialPort serialPort;
+	public Map<SPCSetting, Object> settings;
 	
+	public static SPCSettings getSettings(SerialPort sp) 	{ return new SPCSettings(sp); }
 	
 	public SPCSettings(SerialPort sp) {
+		this.serialPort = sp;
+	}
+	
+	public Object getSetting(SPCSetting setting) {
+		return SPCSettings.fetchSetting(setting, serialPort);
+	}
+	
+	public void setSetting(SPCSetting setting, SerialPort sp, Object value) {
+		assert value.getClass().isInstance(setting.clas) : "trying to assign mismatched objects";
+	}
+	
+	public static Object fetchSetting(SPCSetting setting, SerialPort sp) {		
+		var v = Enum.class;
 		
+		switch(setting) {
+			case SYSTEM_PORT_NAME : 						return sp.getSystemPortName();
+			case SYSTEM_PORT_PATH : 						return sp.getSystemPortPath();
+			case SYSTEM_PORT_LOCATION : 					return sp.getPortLocation();
+			case DESCRIPTIVE_PORT_NAME : 					return sp.getDescriptivePortName();
+			case PORT_DESCRIPTION : 						return sp.getPortDescription();
+			case PORT_LOCATION : 							return sp.getPortLocation();
+			case BAUD_RATE : 								return sp.getBaudRate();
+			case DEVICE_WRITE_BUFFER_SIZE : 				return sp.getDeviceWriteBufferSize();
+			case DEVICE_READ_BUFFER_SIZE : 					return sp.getDeviceReadBufferSize();
+			case VENDOR_ID : 								return sp.getVendorID();
+			case DATA_BITS_PER_WORD : 						return sp.getNumDataBits();
+			case NUM_STOP_BITS : 							return sp.getNumStopBits();
+			case PARITY : 									return sp.getParity();
+			case TIMEOUT_READ : 							return sp.getReadTimeout();
+			case TIMEOUT_WRITE : 							return sp.getWriteTimeout();
+			case FLOWCONTROL_DATA_SET_READY_ENABLED :		return (SerialPort.FLOW_CONTROL_DSR_ENABLED 		& sp.getFlowControlSettings()) == 1;
+			case FLOWCONTROL_DATA_TERMINAL_READY_ENABLED : 	return (SerialPort.FLOW_CONTROL_DTR_ENABLED 		& sp.getFlowControlSettings()) == 1;
+			case FLOWCONTROL_XIN_ONOFF_ENABLED : 			return (SerialPort.FLOW_CONTROL_XONXOFF_IN_ENABLED 	& sp.getFlowControlSettings()) == 1;
+			case FLOWCONTROL_XOUT_ONOFF_ENABLED : 			return (SerialPort.FLOW_CONTROL_XONXOFF_OUT_ENABLED & sp.getFlowControlSettings()) == 1;
+			case FLOWCONTROL_REQUEST_TO_SEND_ENABLED : 		return (SerialPort.FLOW_CONTROL_RTS_ENABLED 		& sp.getFlowControlSettings()) == 1;
+			case FLOWCONTROL_CLEAR_TO_SEND_ENABLED : 		return (SerialPort.FLOW_CONTROL_CTS_ENABLED 		& sp.getFlowControlSettings()) == 1;
+
+			default:
+				System.out.println("SPCSetting>fetchSetting : failed to find setting" + setting.toString());
+				return null;
+		}
 	}
 }
+
+/*
+ switch(setting) {
+			case SYSTEM_PORT_NAME : 
+				return null;
+			case SYSTEM_PORT_PATH : 
+				return null;
+			case SYSTEM_PORT_LOCATION : 
+				return null;
+			case DESCRIPTIVE_PORT_NAME : 
+				return null;
+			case PORT_DESCRIPTION : 
+				return null;
+			case PORT_LOCATION : 
+				return null;
+			case BAUD_RATE : 
+				return null;
+			case DEVICE_WRITE_BUFFER_SIZE : 
+				return null;
+			case DEVICE_READ_BUFFER_SIZE : 
+				return null;
+			case VENDOR_ID : 
+				return null;
+			case DATA_BITS_PER_WORD : 
+				return null;
+			case NUM_STOP_BITS : 
+				return null;
+			case PARITY : 
+				return null;
+			case TIMEOUT_READ : 
+				return null;
+			case TIMEOUT_WRITE : 
+				return null;
+			case FLOWCONTROL_DATA_SET_READY_ENABLED : 
+				return null;
+			case FLOWCONTROL_DATA_TERMINAL_READY_ENABLED : 
+				return null;
+			case FLOWCONTROL_XIN_ONOFF_ENABLED : 
+				return null;
+			case FLOWCONTROL_XOUT_ONOFF_ENABLED : 
+				return null;
+			case FLOWCONTROL_REQUEST_TO_SEND_ENABLED : 
+				return null;
+			case FLOWCONTROL_CLEAR_TO_SEND_ENABLED : 
+				return null;
+
+			default:
+				System.out.println("SPCSetting>function : failed to match setting" + setting.toString());
+				return null;
+		}
+ * /
+ */
